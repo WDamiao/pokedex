77b82c8f87403e3c186cf0e48955e01be6a23a37
Wesley <w.damiao15@gmail.com>
2022-09-13 17:00:54 -0300

    mudanca na estrutura do modal que agora mostra a carta do respectivo pokemon

--- app.js
+++ app.js
@@ -61,27 +61,6 @@ var array = [
     }
 ];
 
-var iconTipo = [
-
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Pok%C3%A9mon_Fire_Type_Icon.svg/120px-Pok%C3%A9mon_Fire_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg/120px-Pok%C3%A9mon_Water_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Pok%C3%A9mon_Grass_Type_Icon.svg/120px-Pok%C3%A9mon_Grass_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Pok%C3%A9mon_Bug_Type_Icon.svg/120px-Pok%C3%A9mon_Bug_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Pok%C3%A9mon_Electric_Type_Icon.svg/120px-Pok%C3%A9mon_Electric_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Pok%C3%A9mon_Flying_Type_Icon.svg/120px-Pok%C3%A9mon_Flying_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Pok%C3%A9mon_Ground_Type_Icon.svg/120px-Pok%C3%A9mon_Ground_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Pok%C3%A9mon_Rock_Type_Icon.svg/120px-Pok%C3%A9mon_Rock_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Pok%C3%A9mon_Ice_Type_Icon.svg/120px-Pok%C3%A9mon_Ice_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Pok%C3%A9mon_Normal_Type_Icon.svg/120px-Pok%C3%A9mon_Normal_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Pok%C3%A9mon_Poison_Type_Icon.svg/120px-Pok%C3%A9mon_Poison_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Pok%C3%A9mon_Ghost_Type_Icon.svg/120px-Pok%C3%A9mon_Ghost_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Pok%C3%A9mon_Fighting_Type_Icon.svg/120px-Pok%C3%A9mon_Fighting_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Pok%C3%A9mon_Psychic_Type_Icon.svg/120px-Pok%C3%A9mon_Psychic_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Pok%C3%A9mon_Steel_Type_Icon.svg/120px-Pok%C3%A9mon_Steel_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Pok%C3%A9mon_Fairy_Type_Icon.svg/120px-Pok%C3%A9mon_Fairy_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Pok%C3%A9mon_Dark_Type_Icon.svg/120px-Pok%C3%A9mon_Dark_Type_Icon.svg.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Pok%C3%A9mon_Dragon_Type_Icon.svg/120px-Pok%C3%A9mon_Dragon_Type_Icon.svg.png"
-]
 let conteudo = document.getElementById("conteudo");
 
 function mostraPokemon() {
@@ -134,26 +113,8 @@ function toggleModal() {//mostra e esconde modal
 }
 
 function preencheModal(pokemon) {
-    let tipoModal = document.querySelector(".tipo");
-    document.querySelector(".tipo").innerHTML = "";
-    for (let i = 0; i < pokemon.tipo.length; i++) {
-        if (pokemon.tipo[i] == "fogo") {
-            tipoModal.src = iconTipo[0]
-        }
-        if (pokemon.tipo[i] == "água") {
-            tipoModal.src = iconTipo[1]
-        }
-        if (pokemon.tipo[i] == "planta") {
-            tipoModal.src = iconTipo[2]
-        }
-        if (pokemon.tipo[i] == "veneno") {
-            tipoModal.src = iconTipo[10]
-        }
-    }
-    let modalImg = document.querySelector(".modalImg");
-    modalImg.src = pokemon.url;
-    let titulo = document.querySelector(".modalNome");
-    titulo.innerHTML = pokemon.name;
+    let carta = document.getElementById("carta");
+    carta.src = pokemon.carta;
 }
 
 conteudo.addEventListener("click", (e) => {
@@ -162,7 +123,6 @@ conteudo.addEventListener("click", (e) => {
         let pokemon = item[0];
         toggleModal();
         preencheModal(pokemon);
-        console.log(pokemon);
     }
 });
 
